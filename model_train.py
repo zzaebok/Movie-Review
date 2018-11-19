aeb5783a1661cc5a7e03e0b830cfc4184326c983
zzaebok <44357534+zzaebok@users.noreply.github.com>
2018-11-19 15:29:56 +0800

    Add files via upload

--- model_train.py
+++ model_train.py
@@ -24,12 +24,20 @@ flags = tf.flags
 flags.DEFINE_integer('num_units', 128, 'number of LSTM units')
 flags.DEFINE_integer('hidden_units', 128, 'number of hidden units in ffn')
 flags.DEFINE_integer('num_classes', 2, 'number of classes')
-flags.DEFINE_integer('epochs', 4, 'epochs')
+flags.DEFINE_integer('epochs', 1, 'epochs')
 flags.DEFINE_integer('batch_size', params['batch_size'], 'batch_size')
 flags.DEFINE_float('lr', params['learning_rate'], 'batch_size')
 flags.DEFINE_string('dataset', params['default_training_data_path'], 'training data path')
 flags.DEFINE_string('testset', params['default_test_data_path'], 'test data path')
 config = flags.FLAGS
+
+with open(config.testset, 'r', encoding='utf8') as f:
+    total_size = len(f.read().split('\n')[1:])
+    test_batch = int(total_size / config.batch_size) + 1
+
+with open(config.dataset, 'r', encoding='utf8') as f:
+    total_size = len(f.read().split('\n')[1:])
+    train_batch = int(total_size / config.batch_size) + 1
 
 word_dict = make_word_dictionary()
 word_emb = make_word_embedding(word_dict)
@@ -42,6 +50,7 @@ with tf.Session(config=sess_config) as sess:
     sess.run(tf.global_variables_initializer())
     saver = tf.train.Saver()
     for epoch in range(config.epochs):
+        total_loss = 0
         for i, data in enumerate(dataset_iterator(config.dataset, word_dict, config.batch_size)):
             feed_dict={
                 model.context : data[0],
@@ -50,14 +59,11 @@ with tf.Session(config=sess_config) as sess:
                 model.labels:data[2]
             }
             _, losses = sess.run([model.train_op, model.loss], feed_dict=feed_dict)
-        print('training loss = ', losses)
+            total_loss += losses/train_batch
+        print('training loss = ', total_loss)
     print('training Done ...')
     print()
     print('Accuracy calculation started ...')
-
-    with open(config.testset, 'r', encoding='utf8') as f:
-        total_size = len(f.read().split('\n')[1:-1])
-        test_batch = int(total_size / 300) + 1
 
     total_acc = 0
     for i,data in enumerate(dataset_iterator(config.testset, word_dict, config.batch_size)):
